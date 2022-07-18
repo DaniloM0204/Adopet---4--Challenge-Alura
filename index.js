76f2b266e1d6c324c0f042c62d593ce0c5b9aabc
DanioM0204 <danilo_mbezerra@hotmail.com>
2022-07-18 17:43:34 -0300

    validação email e senha

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
-const cadastroPessoa = document.querySelector('.form-control');
-const inputName = cadastroPessoa.querySelector('#nome');
-const inputEmail = cadastroPessoa.querySelector('#email');
-const inputPassword = cadastroPessoa.querySelector('#senha');
-const submitForm = document.querySelector('.btn-cadastro');
+const cadastroPessoa = document.querySelector(".form-control");
+const submitForm = document.querySelector('.btn-login');
+const inputSenha = cadastroPessoa.querySelector('.senha'); 
+const inputEmail = cadastroPessoa.querySelector('.email');
 
-const pessoas = JSON.parse(localStorage.getItem('pessoas')) || []
+const pessoas = JSON.parse(localStorage.getItem('pessoas')) || [];
 
 const handleSubmitForm = (event) => {
-  event.preventDefault()
-  console.log({ name: inputName.value, email: inputEmail.value})
-  if (!inputName.validity.valid) {
-    alert('Nome invalido');
-  }
-  if (!inputEmail.validity.valid) {
-    alert('Email invalido');
-  }
-  if (!inputPassword.validity.valid) {
-    alert('Senha invalido');
-  } else inputName.validity.valid && inputEmail.validity.valid
-  {
-    pessoas.push({ nome: inputName.value, email: inputEmail.value })
-    localStorage.setItem('pessoas', JSON.stringify(pessoas));
-
-    cadastroPessoa.reset();
-  }
+    event.preventDefault();
+    console.log({ name: inputSenha.value, email: inputEmail.value});
+    if (!inputSenha.validity.valid) {
+        alert('Senha invalida');
+    } if (!inputEmail.validity.valid) {
+        alert('Email invalido');
+    } else (inputSenha.validity.valid && inputEmail.validity.valid);{
+        pessoas.push({nome: inputSenha.value, email: inputEmail.value});
+        localStorage.setItem('pessoas', JSON.stringify(pessoas));
+        
+        cadastroPessoa.reset();
+    }
 }
-
-cadastroPessoa.addEventListener('submit', handleSubmitForm);
