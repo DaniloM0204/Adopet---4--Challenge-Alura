24244194707695333fdcb67eca5f5bcba8283049
DanioM0204 <danilo_mbezerra@hotmail.com>
2022-07-21 12:05:23 -0300

    Atualizações layout

---
+++ js/cadastro.js
@@ -0,0 +1,6 @@
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const form = document.querySelector('.form-control');
+
+const usuarioEmail = localStorage.getItem('email');
+const usuarioSenha = localStorage.getItem('senha');
